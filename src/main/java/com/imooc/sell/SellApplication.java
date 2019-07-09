dacb2ba1db7f693fe9a160e255b384eb7ab01dbb
采花少年 <995574569@qq.com>
2019-07-09 16:11:53 +0800

    bug测试

--- src/main/java/com/imooc/sell/SellApplication.java
+++ src/main/java/com/imooc/sell/SellApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SellApplication {
 
     public static void main(String[] args) {
-        System.out.print("=========");
+        System.out.print("=========25");
         SpringApplication.run(SellApplication.class, args);
     }
 
